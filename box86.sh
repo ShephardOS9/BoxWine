6e5d1cdc184228f324a80e172ab65d87e71f1e7f
ShephardOS9 <shephardos962@gmail.com>
2025-08-01 15:08:12 +0500

    Update box86.sh

--- box86.sh
+++ box86.sh
@@ -46,45 +46,44 @@ fi
 echo "Installing WoW64 version..."
 sleep 3
 
+wget -q --show-progress -O $HOME/box64-binaries.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/box64-binaries.tar.xz
+tar -xf $HOME/box64-binaries.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/box64-binaries.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/box64-binaries.tar.xz
-tar -xvzf $HOME/box64-binaries.tar.xz -C $PREFIX 
+wget -q --show-progress -O $HOME/dxvk.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/dxvk.tar.xz
+tar -xf $HOME/dxvk.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/dxvk.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/dxvk.tar.xz
-tar -xvzf $HOME/dxvk.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/en-ru-locate.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/en-ru-locate.tar.xz
+tar -xf $HOME/en-ru-locate.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/en-ru-locate.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/en-ru-locate.tar.xz
-tar -xvzf $HOME/en-ru-locate.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/glibc-prefix.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/glibc-prefix.tar.xz
+tar -xf $HOME/glibc-prefix.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/glibc-prefix.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/glibc-prefix.tar.xz
-tar -xvzf $HOME/glibc-prefix.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/glibc_package_manager.tar.gz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/glibc_package_manager.tar.gz
+tar -xf $HOME/glibc_package_manager.tar.gz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/glibc_package_manager.tar.gz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/glibc_package_manager.tar.gz
-tar -xvzf $HOME/glibc_package_manager.tar.gz -C $PREFIX
+wget -q --show-progress -O $HOME/libudev.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/libudev.tar.xz
+tar -xf $HOME/libudev.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/libudev.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/libudev.tar.xz
-tar -xvzf $HOME/libudev.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/package-manager https://github.com/ShephardOS9/BoxWine/releases/download/Box86/package-manager
+tar -xf $HOME/package-manager -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/package-manager https://github.com/ShephardOS9/releases/download/Box86/package-manager
-tar -xvzf $HOME/package-manager -C $PREFIX
+wget -q --show-progress -O $HOME/prefix-apps.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/prefix-apps.tar.xz
+tar -xf $HOME/prefix-apps.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/prefix-apps.tar.xz https://github.com/ShephardOS9/releases/download/Box86/prefix-apps.tar.xz
-tar -xvzf $HOME/prefix-apps.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/scripts.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/scripts.tar.xz
+tar -xf $HOME/scripts.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/scripts.tar.xz https://github.com/ShephardOS9/releases/download/Box86/scripts.tar.xz
-tar -xvzf $HOME/scripts.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/turnip.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/turnip.tar.xz
+tar -xf $HOME/turnip.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/turnip.tar.xz https://github.com/ShephardOS9/releases/download/Box86/turnip.tar.xz
-tar -xvzf $HOME/turnip.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/virgl-mesa.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/virgl-mesa.tar.xz
+tar -xf $HOME/virgl-mesa.tar.xz -C $PREFIX
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/virgl-mesa.tar.xz https://github.com/ShephardOS9/releases/download/Box86/virgl-mesa.tar.xz
-tar -xvzf $HOME/virgl-mesa.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/wine-ge-custom-8-25.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/wine-ge-custom-8-25.tar.xz
+tar -xf $HOME/wine-ge-custom-8-25.tar.xz
 
-wget -q --show-progress -O /data/data/com.termux/files/usr/wine-ge-custom-8-25.tar.xz https://github.com/ShephardOS9/releases/download/Box86/wine-ge-custom-8-25.tar.xz
-tar -xvzf $HOME/wine-ge-custom-8-25.tar.xz
-
-wget -q --show-progress -O /data/data/com.termux/files/usr/wined3d.tar.xz https://github.com/ShephardOS9/releases/download/Box86/wined3d.tar.xz
-tar -xvzf $HOME/wined3d.tar.xz -C $PREFIX
+wget -q --show-progress -O $HOME/wined3d.tar.xz https://github.com/ShephardOS9/BoxWine/releases/download/Box86/wined3d.tar.xz
+tar -xf $HOME/wined3d.tar.xz -C $PREFIX
 
 fi
 . $PREFIX/glibc/opt/package-manager/package-manager
