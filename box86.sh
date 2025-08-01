f4c24831bf88c3327d0f1acdf0089ca20d187b9e
ShephardOS9 <shephardos962@gmail.com>
2025-08-01 11:49:41 +0500

    Update box86.sh

--- box86.sh
+++ box86.sh
@@ -46,3 +46,6 @@ fi
 echo "Installing WoW64 version..."
 sleep 3
 
+wget -q --show-progress https://github.com/ShephardOS9/BoxWine/releases/download/Box86/box64-binaries.tar.xz
+
+wget -q --show-progress https://github.com/ShephardOS/BoxWine/realeases/download/Box86/
